a922eb78368f9a5968a32f61c9d327009846464c
Prashant kumar <prashantkumar50@hotmail.com>
2023-05-14 23:28:52 +0530

    added a mux router

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/aws/aws-lambda-go v1.41.0
 	github.com/aws/aws-sdk-go v1.44.262
+	github.com/awslabs/aws-lambda-go-api-proxy v0.14.0
 	github.com/gorilla/mux v1.8.0
 )
 
--- mediaHandler.go
+++ mediaHandler.go
@@ -2,26 +2,34 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
+	"github.com/gorilla/mux"
 )
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	switch request.Path {
-	case "/post/create":
-		return createPost(ctx, request)
-	default:
-		return events.APIGatewayProxyResponse{Body: "Not Found", StatusCode: 404}, nil
-	}
+var adapter *gorillamux.GorillaMuxAdapterV2
+
+func init() {
+	r := mux.NewRouter()
+	r.HandleFunc("/post/create", createPostHandler).Methods("POST")
+	adapter = gorillamux.NewV2(r)
 }
 
-func createPost(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	response := events.APIGatewayProxyResponse{
-		Body:       "Hoka is such a bitch!",
-		StatusCode: 200,
+func handler(ctx context.Context, request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	res, err := adapter.ProxyWithContext(ctx, request)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
-	return response, nil
+	return res, err
+}
+
+func createPostHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("damn son, that is something"))
 }
 
 func main() {
