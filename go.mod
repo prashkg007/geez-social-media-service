b15552220c95ef388546034a6b5a0f26e59b130a
Prashant kumar <prashantkumar50@hotmail.com>
2023-05-14 20:36:21 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module main
+
+go 1.18
+
+require (
+	github.com/aws/aws-lambda-go v1.41.0
+	github.com/aws/aws-sdk-go v1.44.262
+	github.com/gorilla/mux v1.8.0
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
---
+++ mediaHandler.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	switch request.Path {
+	case "/post/create":
+		return events.APIGatewayProxyResponse{Body: "Hello, World!", StatusCode: 200}, nil
+	default:
+		return events.APIGatewayProxyResponse{Body: "Not Found", StatusCode: 404}, nil
+	}
+}
+
+func createPost(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	response := events.APIGatewayProxyResponse{
+		Body:       "Hoka is such a bitch!",
+		StatusCode: 200,
+	}
+	return response, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
